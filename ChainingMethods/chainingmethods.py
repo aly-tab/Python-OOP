a4c637d3cb5481ef417b8115cf54c81d4c8917ed
w-phil <wphillips892@gmail.com>
2021-07-04 17:33:20 -0400

    chaining methods

--- ChainingMethods/chainingmethods.py
+++ ChainingMethods/chainingmethods.py
@@ -5,16 +5,20 @@ class User:
         self.account_balance = 0
 
     def make_deposit(self, amount):	
-    	self.account_balance += amount	
+        self.account_balance += amount
+        return self	
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print("User:" , self.name)
         print("Balance:" , self.account_balance)
+        return self
     # def transfer(self, other_user, amount)
     def transfer_money(self, other_user, amount): 
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 pete = User("Pete Dort" , "pete@python.com")
 jim = User("Jim Dean", "jim@python.com")
@@ -24,25 +28,12 @@ print(pete.name)
 print(jim.name)
 print(george.name)
 
-pete.make_deposit(50)
-pete.make_deposit(150)
-pete.make_deposit(100)
-pete.make_withdrawal(75)
-pete.display_user_balance()
+pete.make_deposit(50).make_deposit(150).make_deposit(100).make_withdrawal(75).display_user_balance()
 
-jim.make_deposit(200)
-jim.make_deposit(10)
-jim.make_withdrawal(85)
-jim.display_user_balance()
+jim.make_deposit(200).make_deposit(10).make_withdrawal(85).display_user_balance()
 
-george.make_deposit(400)
-george.make_withdrawal(50)
-george.make_withdrawal(5)
-george.make_withdrawal(15)
+pete.transfer_money(george, 100).display_user_balance()
 george.display_user_balance()
 
-pete.transfer_money(george, 100)
-pete.display_user_balance()
-pete.display_user_balance()
 
 
